d9c24febaedf4b7494634a6c927da34f6259a124
AshwinR <ashwinr@ubuntu.(none)>
2014-04-17 10:29:37 -0700

    Near Final Commit

--- flask_tracking/tracking/models.py
+++ flask_tracking/tracking/models.py
@@ -36,7 +36,22 @@ class Entry(CRUDMixin, db.Model):
     def __str__(self):
         return str(self.punchOut)
 
+class Visit(CRUDMixin, db.Model):
+    __tablename__ = 'tracking_visit'
 
+    browser = db.Column(db.String)
+    date = db.Column(db.DateTime)
+    event = db.Column(db.String)
+    url = db.Column(db.String)
+    ip_address = db.Column(db.String)
+    location = db.Column(db.String)
+    latitude = db.Column(db.Numeric)
+    longitude = db.Column(db.Numeric)
+    site_id = db.Column(db.Integer, db.ForeignKey('tracking_site.id'))
+
+    def __repr__(self):
+        r = '<Visit for site ID {:d}: {} - {:%Y-%m-%d %H:%M:%S}>'
+        return r.format(self.site_id, self.url, self.date)
 
 
 class Site(CRUDMixin, db.Model):
